f8606526d3eb0ad416d956c03aee411dec634399
Manjeet Singh <98460132+manjeetdeveloper@users.noreply.github.com>
2023-10-29 11:15:06 +0530

    new updated

--- theme-clock/script.js
+++ theme-clock/script.js
@@ -3,12 +3,12 @@ const minuteEl = document.querySelector('.minute')
 const secondEl = document.querySelector('.second')
 const timeEl = document.querySelector('.time')
 const dateEl = document.querySelector('.date')
-const toggle = document.querySelector('.toggle')
+const btn = document.querySelector('.btn')
 
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 
-toggle.addEventListener('click', (e) => {
+btn.addEventListener('click', (e) => {
     const html = document.querySelector('html')
     if (html.classList.contains('dark')) {
         html.classList.remove('dark')
